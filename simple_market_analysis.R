2d6b96699fb62f74739d2c70d68458d7ec0f9f6f
Łukasz Susłowicz <susel.invest@gmail.com>
2024-06-05 18:39:10 +0200

    acf and pacf - first look in data

--- simple_market_analysis.R
+++ simple_market_analysis.R
@@ -2,6 +2,39 @@ library(forecast)
 # install.packages("quantmod")
 library(quantmod)
 
-getSymbols(Symbols = "^GSPC", src = "yahoo", from = "2014-01-01", to = "2024-06-03")
+# Pobranie danych
+getSymbols(Symbols = "^GSPC", src = "yahoo", from = "2004-01-01", to = "2024-06-03")
+
+# Pobranie danych jako time series
+#getSymbols(Symbols = "^GSPC", src = "yahoo", from = "2014-01-01", to = "2024-06-03",
+#           return.class = "ts")
+
+# Format datasetu
 #GSPC - GSPC.Open GSPC.High GSPC.Low GSPC.Close GSPC.Volume GSPC.Adjusted
+
+sp500.close <- GSPC[, "GSPC.Close"]
+sp500.close[ 1:10, ]
+
+plot(sp500.close, main = "Wykres S&P 500 - 20 lat")
+class(sp500.close) # xts, zoo
+
+library(lattice)
+xyplot(sp500.close, aspect = 1 /3)
+#Panelowy: 
+# xyplot(sp500.close, strip = TRUE, cut = list(number = 3, overlap = 0.333))
+
+#install.packages("expsmooth")
+# library(expsmooth)
+
+## Wykresy autokorelacji:
+lag.plot(sp500.close, lags = 12, do.lines = FALSE, main = "Wykres lag.plot dla SP500 z 20 lat")
+
+par(mfrow = c(2, 1))
+Acf(sp500.close, main = "Funkcja ACF")
+Pacf(sp500.close, main = "Funkcja PACF")
+# ACF wskazuje tutaj na silny trend
+
+
+
+
 
