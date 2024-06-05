20905e4361d57a2dc094798d15875df62af25a05
Łukasz Susłowicz <susel.invest@gmail.com>
2024-06-05 16:33:36 +0200

    data of SP500 loaded from yahoo

--- simple_market_analysis.R
+++ simple_market_analysis.R
@@ -1,1 +1,7 @@
 library(forecast)
+# install.packages("quantmod")
+library(quantmod)
+
+getSymbols(Symbols = "^GSPC", src = "yahoo", from = "2014-01-01", to = "2024-06-03")
+#GSPC - GSPC.Open GSPC.High GSPC.Low GSPC.Close GSPC.Volume GSPC.Adjusted
+
